e988ea102ecff20fc972f6314af8dc8c465d84a8
williammq <67214886+williammq@users.noreply.github.com>
2023-04-12 17:14:49 -0500

    change configuration yaml

--- user-service/src/main/java/com/tutorial/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/tutorial/userservice/UserServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 @EnableEurekaClient
+@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
